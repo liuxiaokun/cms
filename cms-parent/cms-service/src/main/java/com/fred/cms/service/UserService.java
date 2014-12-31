50cc70e2cc9971ad25a962ec7967e0e4e05c5e14
fred <fredliu@augmentum.com.cn>
2014-12-31 17:02:53 +0800

    add cms-service module

---
+++ cms-parent/cms-service/src/main/java/com/fred/cms/service/UserService.java
@@ -0,0 +1,4 @@
+package com.fred.cms.service;
+
+public interface UserService {
+}
---
+++ cms-parent/cms-service/src/main/java/com/fred/cms/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.fred.cms.service.impl;
+
+import com.fred.cms.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+}
